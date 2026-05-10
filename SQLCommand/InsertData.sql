5c9b5a1b7ef2661cfc4dc7c2b489e8998b8d396f
joakimran5 <joakimran5@gmail.com>
2026-05-11 00:30:09 +0800

    Update InsertData.sql
    date fromat for insert must be year-month-day

--- SQLCommand/InsertData.sql
+++ SQLCommand/InsertData.sql
@@ -4,7 +4,7 @@ SELECT *
 INSERT INTO [FinanceWarehouseAnalytics].[bank].[acc1_tsc]
 ( tsc_dt, tsc_descrp, tsc_amt, tsc_type, amt_bal)
 VALUES
-( '02/04/2026', 'petrol', 60, 'DBT', 1414.77);
+( '2026/04/02', 'petrol', 60, 'DBT', 1414.77);
 
 --DELETE FROM [FinanceWarehouseAnalytics].[bank].[acc1_tsc] 
 --WHERE tsc_id = 2;
