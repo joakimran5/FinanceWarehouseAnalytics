d987120bd0e0cece35614d8811344ca1f337377c
joakimran5 <joakimran5@gmail.com>
2026-05-10 14:11:34 +0800

    Clean version
    add investment table
    organize as code able to run.
    Ctaegorized code with clear explanation

--- tables/table.sql
+++ tables/table.sql
@@ -1,3 +1,25 @@
+-------------------------BANK ACCOUNT-------------------------
+/*Drop all tables that associate with Account as Foreign Key*/
+IF OBJECT_ID ('wallet.rsv_csh', 'U') IS NOT NULL
+	DROP TABLE wallet.rsv_csh;
+IF OBJECT_ID ('invest.asb_tsc', 'U') IS NOT NULL
+	DROP TABLE invest.asb_tsc;
+IF OBJECT_ID ('debt.ln_tsc', 'U') IS NOT NULL
+	DROP TABLE debt.ln_tsc;
+/*create Table for one Bank Account*/
+IF OBJECT_ID ('bank.acc1_tsc', 'U') IS NOT NULL
+	DROP TABLE bank.acc1_tsc;
+CREATE TABLE bank.acc1_tsc (
+	tsc_id INT PRIMARY KEY IDENTITY(1,1),
+	tsc_dt DATE,
+	tsc_descrp NVARCHAR(50),
+	tsc_amt DECIMAL(8,2),
+	tsc_type NVARCHAR(3),
+	amt_bal DECIMAL(8,2)
+);
+/**************************************************************/
+----------------------WALLET-------------------------------------
+/*Create table for E-wallet */
 IF OBJECT_ID ('wallet.tng_tsc', 'U') IS NOT NULL
 	DROP TABLE wallet.tng_tsc;
 CREATE TABLE wallet.tng_tsc (
@@ -8,9 +30,7 @@ CREATE TABLE wallet.tng_tsc (
 	tsc_type NVARCHAR(3),
 	amt_bal DECIMAL(8,2)
 );
-
-IF OBJECT_ID ('wallet.rsv_csh', 'U') IS NOT NULL
-	DROP TABLE wallet.rsv_csh;
+/*Create table for Reserve Cash */
 CREATE TABLE wallet.rsv_csh (
 	rsv_id INT PRIMARY KEY IDENTITY(1,1),
 	rsv_dt DATE,
@@ -25,30 +45,22 @@ CREATE TABLE wallet.rsv_csh (
 		FOREIGN KEY (tsc_id)
 		REFERENCES bank.acc1_tsc(tsc_id)
 );
-
-
-
-IF OBJECT_ID ('debt.ln_tsc', 'U') IS NOT NULL
-	DROP TABLE debt.ln_tsc;
-CREATE TABLE debt.ln_tsc (
-	ln_tsc_id INT PRIMARY KEY IDENTITY(1,1),
-	tsc_id INT,
-	ln_id INT,
-	tsc_amt DECIMAL(8,2),
-	amt_bal DECIMAL(8,2), 
-	ln_out DECIMAL(8,2),
-		FOREIGN KEY (ln_id)
-				REFERENCES debt.ln_acc_info(ln_id),
-		FOREIGN KEY (tsc_id)
-		REFERENCES bank.acc1_tsc(tsc_id)
+/***************************************************************/
+----------------------INVESTMENT---------------------------------
+/*Create table for ASB account info*/
+IF OBJECT_ID ('invest.asb_acc_info', 'U') IS NOT NULL
+	DROP TABLE invest.asb_acc_info;
+CREATE TABLE invest.asb_acc_info (
+	asb_id INT PRIMARY KEY IDENTITY(1,1),
+	asb_code VARCHAR(10) UNIQUE,
+	asb_name VARCHAR(50),
+	asb_year INT,
+	asb_rt DECIMAL(4,2),
+	asb_type VARCHAR(10),
+	asb_amt DECIMAL(9,2)
 );
 
-
-
-
-
-IF OBJECT_ID ('invest.asb_tsc', 'U') IS NOT NULL
-	DROP TABLE invest.asb_tsc;
+/*Create table for ASB transactions */
 CREATE TABLE invest.asb_tsc (
 	asb_tsc_id INT PRIMARY KEY IDENTITY(1,1),
 	asb_id INT,
@@ -61,11 +73,9 @@ CREATE TABLE invest.asb_tsc (
 		FOREIGN KEY (bank_tsc_id)
 			REFERENCES bank.acc1_tsc(tsc_id)
 );
-
-
-
-
-
+/***************************************************************/
+-------------------------DEBT-----------------------------------
+/*Create table for loan account info*/
 IF OBJECT_ID ('debt.ln_acc_info', 'U') IS NOT NULL
 	DROP TABLE debt.ln_acc_info;
 CREATE TABLE debt.ln_acc_info (
@@ -76,26 +86,17 @@ CREATE TABLE debt.ln_acc_info (
 	ln_dur INT,
 	ln_rt DECIMAL(4,2)
 );
-
-IF OBJECT_ID ('bank.acc1_tsc', 'U') IS NOT NULL
-	DROP TABLE bank.acc1_tsc;
-CREATE TABLE bank.acc1_tsc (
-	tsc_id INT PRIMARY KEY IDENTITY(1,1),
-	tsc_dt DATE,
-	tsc_descrp NVARCHAR(50),
+/*Create table for loan transactions*/
+CREATE TABLE debt.ln_tsc (
+	ln_tsc_id INT PRIMARY KEY IDENTITY(1,1),
+	tsc_id INT,
+	ln_id INT,
 	tsc_amt DECIMAL(8,2),
-	tsc_type NVARCHAR(3),
-	amt_bal DECIMAL(8,2)
+	amt_bal DECIMAL(8,2), 
+	ln_out DECIMAL(8,2),
+		FOREIGN KEY (ln_id)
+				REFERENCES debt.ln_acc_info(ln_id),
+		FOREIGN KEY (tsc_id)
+		REFERENCES bank.acc1_tsc(tsc_id)
 );
-
-IF OBJECT_ID ('invest.asb_acc_info', 'U') IS NOT NULL
-	DROP TABLE invest.asb_acc_info;
-CREATE TABLE invest.asb_acc_info (
-	asb_id INT PRIMARY KEY IDENTITY(1,1),
-	asb_code VARCHAR(10) UNIQUE,
-	asb_name VARCHAR(50),
-	asb_year INT,
-	asb_rt DECIMAL(4,2),
-	asb_type VARCHAR(10),
-	asb_amt DECIMAL(9,2)
-);
+/*****************************************************/
