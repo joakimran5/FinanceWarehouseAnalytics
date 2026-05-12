e6b82e002c6e830c5c77c93f48d6934479a17104
joakimran5 <joakimran5@gmail.com>
2026-05-12 13:24:05 +0800

    Update InsertData.sql
    format to insert table, theres is added column for category of transaction to make it more clear for analysis

--- SQLCommand/InsertData.sql
+++ SQLCommand/InsertData.sql
@@ -1,10 +1,17 @@
 SELECT *
   FROM [FinanceWarehouseAnalytics].[bank].[acc1_tsc]
-
 INSERT INTO [FinanceWarehouseAnalytics].[bank].[acc1_tsc]
-( tsc_dt, tsc_descrp, tsc_amt, tsc_type, amt_bal)
+  
+( tsc_dt, tsc_cat,tsc_descrp, tsc_amt, tsc_type, amt_bal)
 VALUES
-( '2026/04/02', 'petrol', 60, 'DBT', 1414.77);
+( '2026/04/04','Food', 'pizza', 0.00, 'DBT', 0.00),
+( '2026/04/05',NULL, NULL, 0.00, 'DBT', 0.00),
+( '2026/04/05','Transportation', 'petrol', 0.00, 'DBT', 0.00),
+( '2026/04/06','Income', 'bolt', 0.00, 'CDT', 0.00),
+( '2026/04/08','Food', 'coffee', 00.00, 'DBT', 0.00),
+( '2026/04/09','Transportation', 'petrol', 0.00, 'DBT', 0.00),
+( '2026/04/11','Transportation', 'psv-license', 0.00, 'DBT', 0.00),
+( '2026/04/12','Transportation', 'petrol', 0.00, 'DBT', 0.00);
 
 --DELETE FROM [FinanceWarehouseAnalytics].[bank].[acc1_tsc] 
 --WHERE tsc_id = 2;
