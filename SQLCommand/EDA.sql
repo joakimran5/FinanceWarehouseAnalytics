c909cc3d17ff67c83532e06c18f3b22573cf7f1f
joakimran5 <joakimran5@gmail.com>
2026-05-23 14:11:43 +0800

    Refactor SQL queries for transportation and income analysis
    adjust some transcation to be more clear, add clumn name as undersatndable to read and understand, adding some analysisi for inocme and transportation, strcutured query for ease of undesrtand and adjust soem query to be more readable and less LOC

--- SQLCommand/EDA.sql
+++ SQLCommand/EDA.sql
@@ -1,52 +1,95 @@
-/****** Script for SelectTop Rows command from SSMS  ******/
-SELECT tsc_dt,tsc_cat, tsc_descrp, tsc_amt,amt_bal
-  FROM [FinanceWarehouseAnalytics].[bank].acc1_tsc
-  where tsc_descrp = 'petrol'
-
----Amount Spend for transportation-------
-(SELECT SUM(tsc_amt) AS total_amt, tsc_descrp
+/**********************BANK****************************************/
+------------------------------------------------------
+----View total amount of credited and debit ----------
+SELECT
+	tsc_type,
+	SUM(tsc_amt) AS total_amt
 FROM [FinanceWarehouseAnalytics].[bank].acc1_tsc
-WHERE tsc_cat = 'transportation' AND not tsc_descrp='petrol'
-group by tsc_descrp)
-UNION
-(SELECT SUM(tsc_amt), tsc_descrp
-FROM [FinanceWarehouseAnalytics].[bank].acc1_tsc
-WHERE tsc_cat = 'transportation'AND tsc_descrp='petrol'
-group by tsc_descrp)
+WHERE tsc_type IN ('CDT', 'DBT')
+GROUP BY tsc_type;
 
----Amount Spend for petrol & sustaining-------
+----View all transaction----------------------------------
+SELECT tsc_dt,tsc_cat, tsc_descrp, tsc_amt
+FROM [FinanceWarehouseAnalytics].[bank].acc1_tsc
+
+-------View transaction based on details---------------
+SELECT
+	tsc_cat,
+	tsc_descrp,
+	SUM(tsc_amt) AS total_amt
+FROM [FinanceWarehouseAnalytics].[bank].acc1_tsc
+GROUP BY tsc_cat, tsc_descrp
+ORDER BY tsc_cat DESC;
+
+-------View transaction based on category-------------
+SELECT
+	tsc_cat,
+	SUM(tsc_amt) AS total_amt
+FROM [FinanceWarehouseAnalytics].[bank].acc1_tsc
+GROUP BY tsc_cat
+ORDER BY tsc_cat DESC;
+
+------------------------------------------------------------------
+/**********************TRANSPORTATION******************************/
+-----------------------------------------------------
+---View sum of Petrol consumption for a month -------
+SELECT
+	SUM(tsc_amt) AS total_petrol_expense
+FROM bank.acc1_tsc
+WHERE tsc_descrp = 'petrol'
+	AND tsc_type = 'DBT'
+	AND MONTH(tsc_dt) = 4
+	AND YEAR(tsc_dt) = 2026;
+
+---Amount Spend for transportation for a month-------
+SELECT sum(tsc_amt), tsc_descrp, tsc_type
+FROM bank.acc1_tsc
+WHERE tsc_cat = 'Transportation' 
+	AND MONTH(tsc_dt) = 4
+	AND YEAR(tsc_dt) = 2026
+group by tsc_descrp, tsc_type;
+
+---Amount Spend for petrol & sustaining car for a month-------
 SELECT 
     SUM(CASE 
             WHEN tsc_cat = 'transportation' 
                  AND tsc_descrp <> 'petrol'
+				 AND MONTH(tsc_dt) = 4
+				 AND YEAR(tsc_dt) = 2026
             THEN tsc_amt
             ELSE 0
-        END) AS sustain_amt,
+        END) AS 'Total Sustaining',
 
     SUM(CASE 
             WHEN tsc_cat = 'transportation' 
                  AND tsc_descrp = 'petrol'
+				 AND MONTH(tsc_dt) = 4
+				 AND YEAR(tsc_dt) = 2026
             THEN tsc_amt
             ELSE 0
-        END) AS petrol_amt
+        END) AS 'Petrol Consumption'
 FROM [FinanceWarehouseAnalytics].[bank].acc1_tsc;
 
-SELECT sum(tsc_amt) AS total_amt, tsc_descrp
+---------------------------------------------------------------
+/**********************INCOME*********************************/
+---------------------------------------------------------------
+--------Total Credit into account----------------------------
+SELECT sum(tsc_amt) AS 'Total Credited'
 FROM [FinanceWarehouseAnalytics].[bank].acc1_tsc
-WHERE tsc_type = 'CDT' AND NOT tsc_descrp= 'Refund-postpaid'
+WHERE MONTH(tsc_dt) = 4 AND YEAR(tsc_dt) = 2026
+
+-------Total Credit into account with Descrption---------------
+SELECT sum(tsc_amt) AS 'Total Credited', tsc_descrp AS 'Details'
+FROM [FinanceWarehouseAnalytics].[bank].acc1_tsc
+WHERE tsc_type = 'CDT'
+		AND MONTH(tsc_dt) = 4
+		AND YEAR(tsc_dt) = 2026
 group by tsc_descrp 
 
+--------Total income into account------------------------------
 SELECT sum(tsc_amt) AS total_income_amt
 FROM [FinanceWarehouseAnalytics].[bank].acc1_tsc
-WHERE tsc_descrp= 'grab' or tsc_descrp= 'bolt'
-
-SELECT sum(tsc_amt) AS total_amt, tsc_descrp
-FROM [FinanceWarehouseAnalytics].[bank].acc1_tsc
-WHERE tsc_type = 'CDT' AND NOT tsc_descrp= 'Refund-postpaid'
-group by tsc_descrp 
-
-SELECT sum(tsc_amt) AS total_income_amt
-FROM [FinanceWarehouseAnalytics].[bank].acc1_tsc
-WHERE tsc_descrp= 'grab' or tsc_descrp= 'bolt'
-
-
+WHERE tsc_type = 'CDT' AND NOT tsc_descrp='Refund-postpaid'
+		AND MONTH(tsc_dt) = 4
+		AND YEAR(tsc_dt) = 2026
+---------------------------------------------------------------
