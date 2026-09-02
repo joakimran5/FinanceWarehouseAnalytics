2f1f662fbeeeae3b5299ac912daa48c05e4303c5
joakimran5 <joakimran5@gmail.com>
2026-09-02 22:10:30 +0800

    Modify primary keys and add rcpt_dtls table
    primary key is copy at bronze layer, rcpt dtls exist at silver layer fro comprehend issses with groceries receipt that need to monitor the price items or any receipt that need more room information

--- tables/silver_table.sql
+++ tables/silver_table.sql
@@ -2,7 +2,7 @@
 IF OBJECT_ID ('silver.acc1_tsc', 'U') IS NOT NULL
 	DROP TABLE silver.acc1_tsc;
 CREATE TABLE silver.acc1_tsc (
-	tsc_id INT PRIMARY KEY IDENTITY(1,1),
+	tsc_id INT PRIMARY KEY,
 	tsc_dt DATE,
 	tsc_cat NVARCHAR(30),
 	tsc_descrp NVARCHAR(50),
@@ -21,7 +21,7 @@ CREATE TABLE silver.acc1_tsc (
 IF OBJECT_ID ('silver.tng_tsc', 'U') IS NOT NULL
 	DROP TABLE silver.tng_tsc;
 CREATE TABLE silver.tng_tsc (
-	tsc_id INT PRIMARY KEY IDENTITY(1,1),
+	tsc_id INT PRIMARY KEY,
 	tsc_dt DATE,
 	tsc_cat NVARCHAR(30),
 	tsc_descrp NVARCHAR(50),
@@ -29,6 +29,24 @@ CREATE TABLE silver.tng_tsc (
 	tsc_type NVARCHAR(3),
 	amt_bal DECIMAL(8,2),
 	rcpt_id INT
+	FOREIGN KEY (rcpt_id)
+		REFERENCES payment.rcpts(rcpt_id)
+);
+
+/*Create  Silver Layer Table for rcpt details*/
+IF OBJECT_ID ('silver.[rcpt_dtls]', 'U') IS NOT NULL
+	DROP TABLE silver.[rcpt_dtls];
+CREATE TABLE silver.rcpt_dtls (
+	rcpt_dtl_id INT PRIMARY KEY,
+	[item_typ] NVARCHAR(50),  --rice
+	[item_brand] NVARCHAR(50), -- JATI ISTIMEWA
+	[item_variants] NVARCHAR(50), -- beras putih/ beras siam/hairfall
+	[item_unit] NVARCHAR(50), --- 10KG/2*140g
+	[item_qty] NVARCHAR(50),  --- 1 pieces
+	[ttl_price] DECIMAL(8,2), -- 38.3 
+	[item_cat]  NVARCHAR(50),  -- dry-goods/dishware/fresh-goods/body-bath
+	item_rmk NVARCHAR(100), -- discount 45%
+	[rcpt_id] INT
 	FOREIGN KEY (rcpt_id)
 		REFERENCES payment.rcpts(rcpt_id)
 );
